5645f9113a2cf4c31bae554dc5b1395f78eb9c60
bedel-dev <bedelcoulibaly8@gmail.com>
2023-09-06 14:06:05 +0000

    contact with azur api

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,3 +1,4 @@
+//Local
 // export class GlobalConstants {
 //   public static apiURL: string = "http://localhost:8788/v1.0/ppai";
 //   public static api_auth: string = "http://localhost:8787/v1.0/users";
@@ -5,8 +6,16 @@
 //   public static host: string = "http://localhost:8788";
 // }
 
+//AWS
+// export class GlobalConstants {
+//     public static apiURL: string = "https://2pai.demoapi.lidar-platform.com/v1.0/ppai";
+//     public static api_auth: string = "https://2pai.demoauthapi.lidar-platform.com/v1.0/users";
+//     public static host: string = "https://2pai.demoapi.lidar-platform.com/";
+// }
+
+//AZURE
 export class GlobalConstants {
-    public static apiURL: string = "https://2pai.demoapi.lidar-platform.com/v1.0/ppai";
-    public static api_auth: string = "https://2pai.demoauthapi.lidar-platform.com/v1.0/users";
-    public static host: string = "https://2pai.demoapi.lidar-platform.com/";
+    public static apiURL: string = "https://ograndmarche-api-rec.azurewebsites.net/app_ppai";
+    public static api_auth: string = "https://ograndmarche-api-rec.azurewebsites.net/app_auth";
+    public static host: string = "https://ograndmarche-api-rec.azurewebsites.net/app_ppai/";
 }
